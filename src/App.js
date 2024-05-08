f89fdde165b8728c523f42d922f9124243832bff
githubsnehak <snehakhandelwal71@gmail.com>
2024-05-08 11:57:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Aboutstart from './components/Aboutstart';
+import Category from './components/Category';
+import Connect from './components/Connect';
+import Client from './components/Client';
+import Property from './components/Property';
+import { BrowserRouter , Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Header/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+     </Routes>
+
+     </BrowserRouter>
+     <Aboutstart/>
+     <Client/>
+     <Connect/>
+     <Property/>
+     <Category/>
+     
+     <Contact/>
+<Footer/>
     </div>
   );
 }
